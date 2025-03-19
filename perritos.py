99bc4ad31e22748887b9a3410deb8a325c0a3011
DiaNat7 <diana.ramirez0783@gmail.com>
2025-03-19 12:23:03 -0600

    Add files via upload

---
+++ perritos.py
@@ -0,0 +1,26 @@
+class perro():
+   # nombre = "Pingo el Dios de la Destrucción"
+   # edad = 8
+   # raza = "labrador"
+   # color = "negro"
+
+    def __init__(self, nombre, edad, raza, color): #privado, variables globales#
+        self.nombre = nombre
+        self.edad = edad
+        self.raza = raza
+        self.color = color
+    def comer(self):
+        print(self.nombre, "está comiendo")
+
+    def dormir(self):
+        print(self.nombre, "está durmiendo")
+
+#dog = perro()
+#dog.comer()
+
+perro_1 = perro("Pingo", 8, "Labrador", "Negro")
+perro_2 =perro("Destructor de asteroides", 6, "Chihuahua", "Café")
+
+perro_1.comer()
+perro_2.dormir()
+
